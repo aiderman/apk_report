32a3ebdc800dac9b8414cdfd4eb0eca3de0a7e9c
aiderman <aiderman347@gmail.com>
2022-02-04 14:08:40 +0800

    navbar

--- application/views/footer.php
+++ application/views/footer.php
@@ -3,8 +3,9 @@
 
 <div class="bottom-navigation bottom-navigation--gradient">
 	<ul class="bottom-navigation__icons">
-	<li><a href="main.html"><img src="<?php echo base_url('assets')?>/images/icons/black/blocks.svg" alt="" title="" /></a></li>
-	<li><a href="user-profile.html"><img src="<?php echo base_url('assets')?>/images/icons/black/user.svg" alt="" title="" /></a></li>
+	<li><a href="#"><img src="<?php echo base_url('assets')?>/images/icons/black/logout.svg" alt="" title="" /></a></li>
+	<li><a href="#"><img src="<?php echo base_url('assets')?>/images/icons/black/blocks.svg" alt="" title="" /></a></li>
+	<li><a href="#"><img src="<?php echo base_url('assets')?>/images/icons/black/user.svg" alt="" title="" /></a></li>
 	<li><a href="#" class="open-popup" data-popup="notifications"><img src="<?php echo base_url('assets')?>/images/icons/black/notifications.svg" alt="" title="" /><i>2</i></a></li>
 	
 	<li class="centered"><a href="forms.html"><img src="<?php echo base_url('assets')?>/images/icons/white/plus-bold.svg" alt="" title="" /></a></li>
--- application/views/navbar.php
+++ application/views/navbar.php
@@ -20,21 +20,21 @@
 		
 		<!-- SLIDER THUMBS 4 round -->
 		
-		<div class="swiper-container slider-thumbs slider-init mb-20 pb-10" data-paginationtype="progressbar" data-spacebetweenitems="10" data-itemsperview="auto" >
+		<div class="swiper-container slider-thumbs slider-init mb-20 pb-10" data-paginationtype="progressbar" data-spacebetweenitems="22" data-itemsperview="auto" >
             <div class="swiper-wrapper">
-				<div class="swiper-slide slider-thumbs__slide slider-thumbs__slide--4h">
+				<!-- <div class="swiper-slide slider-thumbs__slide slider-thumbs__slide--4h">
 					<div class="slider-thumbs__icon slider-thumbs__icon--round-corners--full"><a href="<?php echo base_url('C_master/add_news')?>"><img src="<?php echo base_url('assets')?>/images/icons/black/plus-bold.svg" alt="" title=""/></a></div>
 					<div class="slider-thumbs__caption caption">
 						<div class="caption__content">
 							<h2 class="caption__title caption__title--smaller caption__title--centered">Aduan baru</h2>
 						</div>
 					</div>
-				</div>     
+				</div>      -->
 				<div class="swiper-slide slider-thumbs__slide slider-thumbs__slide--4h">
 					<div class="slider-thumbs__icon slider-thumbs__icon--round-corners--full"><a href="<?php echo base_url('C_master/')?>"><img src="<?php echo base_url('assets')?>/images/icons/red/news.svg" alt="" title=""/></a></div>
 					<div class="slider-thumbs__caption caption">
 						<div class="caption__content">
-							<h2 class="caption__title caption__title--smaller caption__title--centered">berita</h2>
+							<h2 class="caption__title caption__title--smaller caption__title--centered">BERITA</h2>
 						</div>
 					</div>
 				</div> 
@@ -42,7 +42,7 @@
 						<div class="slider-thumbs__icon slider-thumbs__icon--round-corners--full"><a href="<?php echo base_url('C_master/tracking')?>"><img src="<?php echo base_url('assets')?>/images/icons/black/location.svg" alt="" title=""/></a></div>
 						<div class="slider-thumbs__caption caption">
 							<div class="caption__content">
-								<h2 class="caption__title caption__title--smaller caption__title--centered">lacak</h2>
+								<h2 class="caption__title caption__title--smaller caption__title--centered">LACAK</h2>
 							</div>
 						</div>
 					</div> 
@@ -50,15 +50,15 @@
 					<div class="slider-thumbs__icon slider-thumbs__icon--round-corners--full"><a href="<?php echo base_url('C_master/galery')?>"><img src="<?php echo base_url('assets')?>/images/icons/red/photos.svg" alt="" title=""/></a></div>
 					<div class="slider-thumbs__caption caption">
 						<div class="caption__content">
-							<h2 class="caption__title caption__title--smaller caption__title--centered">galeri</h2>
+							<h2 class="caption__title caption__title--smaller caption__title--centered">MEDIA</h2>
 						</div>
 					</div>
 				</div> 
 				<div class="swiper-slide slider-thumbs__slide slider-thumbs__slide--4h">
-					<div class="slider-thumbs__icon slider-thumbs__icon--round-corners--full"><a href="<?php echo base_url('C_master/login')?>"><img src="<?php echo base_url('assets')?>/images/icons/red/logout.svg" alt="" title=""/></a></div>
+					<div class="slider-thumbs__icon slider-thumbs__icon--round-corners--full"><a href="<?php echo base_url('C_master/login')?>"><img src="<?php echo base_url('assets')?>/images/icons/black/faq.svg" alt="" title=""/></a></div>
 					<div class="slider-thumbs__caption caption">
 						<div class="caption__content">
-							<h2 class="caption__title caption__title--smaller caption__title--centered">Keluar</h2>
+							<h2 class="caption__title caption__title--smaller caption__title--centered">FAQ</h2>
 						</div>
 					</div>
 				</div> 
